ec8e0754fb485fed9bb2c7e0b2f0169b66e4caa6
beeyaaa <ebhong01@gmail.com>
2023-08-12 11:59:55 +0900

    fixed ultrasound example

--- arduino/module_test/ultrasound/ultrasound.ino
+++ arduino/module_test/ultrasound/ultrasound.ino
@@ -1,7 +1,6 @@
 #define PIN_US_ECHO 7
 #define PIN_US_TRIG 8
 
-int output_data = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -11,11 +10,15 @@ void setup() {
 
 void loop() {
   float cycletime;
-  float distance;
+  float distance = 0;
+
+  digitalWrite(PIN_US_TRIG, HIGH);
+  delay(10);
+  digitalWrite(PIN_US_TRIG,LOW);
 
   cycletime = pulseIn(PIN_US_ECHO, HIGH); 
   distance = ((340 * cycletime) / 10000) / 2;  
   
-  Serial.println(output_data++);
+  Serial.println(distance);
   delay(500);
 }
